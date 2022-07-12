fdf947aa9403271c21070bca64acd93b249a4d56
kosuke <mowmow.meganekun@gmail.com>
2022-07-12 21:46:42 +0900

    ファイル名変更処理修正

--- bin/ReplaceFileName.ps1
+++ bin/ReplaceFileName.ps1
@@ -1,10 +1,16 @@
 # description and list target files
-Write-Host "拡張子がxlsx,pptx,docx,txtのファイル名の一部を置換" -ForegroundColor Green
-Get-ChildItem -File -Recurse -Include *.xlsx,*.pptx,*.docx,*.txt
+Write-Host "ファイル名置換" -ForegroundColor Green
+Get-ChildItem -Path ".\*"
 
 # replace file name
-$targetStr = Read-Host "置換対象の文字列を入力"
-$replacedStr = Read-Host "置換後の文字列を入力"
-Get-ChildItem -File -Recurse -Include *.xlsx,*.pptx,*.docx,*.txt | Rename-Item -NewName {$_.Name -replace $targetStr, $replacedStr}
+Write-Output ""
+$targetStr = $(Write-Host "置換対象の文字列を入力: " -ForeGroundColor Green -NoNewline; Read-Host)
+$replacedStr = $(Write-Host "置換後の文字列を入力: " -ForeGroundColor Green -NoNewline; Read-Host)
+$files = (Get-ChildItem -Path ".\*")
+foreach ($file in $files) {
+    $newName = $file.Name.Replace($targetStr, $replacedStr)
+    Rename-Item -Path $file.FullName -NewName $newName
+}
+
 Write-Host ""
-Get-ChildItem -File -Recurse -Include *.xlsx,*.pptx,*.docx,*.txt
+Get-ChildItem -Path ".\*"
